48eccef054b09753c9f66fa36dbd2b507cbf6524
bhawesh gupta <bhawesh1996@outlook.com>
2020-07-08 02:20:10 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string str = "hello world";
+            //string result = str.ChangeFirstLetterCase();
+            string result=StringHelper.ChangeFirstLetterCase(str);
+            string newresult = str.ChangeFirstLetterCase();
+
+          //  Console.WriteLine(result);
+        //    Console.WriteLine(newresult);
+
+
+
+            List<int> number = new List<int> { 1, 2, 3,4,5};
+            IEnumerable<int> s=number.Where(n => n % 2 == 0);
+            //this can be written as
+            IEnumerable<int> ss=Enumerable.Where(number,n => n % 2 == 0);
+
+
+
+            foreach(var i in s)
+                Console.WriteLine(i);
+
+            var x=number.Select(n => n > 2);
+            foreach (var item in x)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
